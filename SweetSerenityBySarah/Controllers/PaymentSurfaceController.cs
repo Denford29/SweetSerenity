f33b741ea28538a3b5518700c08880f5eaa43738
Denford29 <denfordmutseriwa@yahoo.com>
2016-07-12 22:41:32 +0930

    inital stripe charge logic

--- SweetSerenityBySarah/Controllers/PaymentSurfaceController.cs
+++ SweetSerenityBySarah/Controllers/PaymentSurfaceController.cs
@@ -14,7 +14,10 @@ namespace SweetSerenityBySarah.Controllers
     {
     public class PaymentSurfaceController : SurfaceController
         {
-
+        /// <summary>
+        /// get the payment form
+        /// </summary>
+        /// <returns></returns>
         public ActionResult PaymentForm()
             {
             var membersList = new List<IMember>();
@@ -55,15 +58,23 @@ namespace SweetSerenityBySarah.Controllers
                 MemberEmailAddress = userEmail,
                 IsUserAdmin = adminUser
             };
+            // ReSharper disable once Mvc.PartialViewNotResolved
             return PartialView("Account/PaymentForm", model);
             }
 
+        /// <summary>
+        /// process the payment form
+        /// </summary>
+        /// <param name="paymentModel"></param>
+        /// <returns></returns>
         public ActionResult ProcessPaymentForm(PaymentFormModel paymentModel)
             {
 
             if (string.IsNullOrWhiteSpace(paymentModel.MemberEmailAddress) &&
                 string.IsNullOrWhiteSpace(paymentModel.MemberId))
                 {
+                TempData["paymentError"] =
+                    "Opps... Payment Form Error, There was an error with your details please check them and try again.";
                 return CurrentUmbracoPage();
                 }
 
@@ -71,45 +82,63 @@ namespace SweetSerenityBySarah.Controllers
 
             if (currentMember == null)
                 {
+                TempData["paymentError"] =
+                    "Opps... Payment Form Error, There was an error with your details please check them and try again.";
                 return CurrentUmbracoPage();
                 }
 
-            var StripeCustomerId = currentMember.GetValue("StripeCustomerId").ToString();
+            var stripeCustomerId = "";
+            var memberFullName = "";
+            var paymentDescription = "Payment for treatment";
+            var typedMember = Umbraco.TypedMember(currentMember.Id);
+            if (typedMember != null && typedMember.Id > 0)
+            {
+                if (typedMember.HasProperty("StripeCustomerId") && typedMember.HasValue("StripeCustomerId"))
+                {
+                stripeCustomerId = typedMember.GetPropertyValue<string>("StripeCustomerId");
+                }
+
+                if (typedMember.HasProperty("fullName") && typedMember.HasValue("fullName"))
+                    {
+                    memberFullName = typedMember.GetPropertyValue<string>("fullName");
+                        paymentDescription += " for : " + memberFullName;
+                    }
+            }
+           
 
             //get the api keys to use from the site settings page
             var siteSetting = Umbraco.TypedContentAtRoot().FirstOrDefault(x => x.ContentType.Alias == "GlobalSettings");
-            var secretAPIKey = PaymentService.GetSecretApiKey(siteSetting);
-            var publishAPIKey = PaymentService.GetPublishApiKey(siteSetting);
+            var secretApiKey = PaymentService.GetSecretApiKey(siteSetting);
+            var publishApiKey = PaymentService.GetPublishApiKey(siteSetting);
 
             // create a new stripe customer with these default values
-            var customerService = new StripeCustomerService(secretAPIKey);
+            var customerService = new StripeCustomerService(secretApiKey);
             StripeCustomer stripeCustomer;
 
             // add the token created earlier to be saved on our customer
-            if (string.IsNullOrWhiteSpace(StripeCustomerId))
-            {
-            var chargedCustomer = new StripeCustomerCreateOptions
-            {
-                Email = currentMember.Email,
-                Description = currentMember.GetValue("fullName")
-                              + " - Payment for treatment.",
-                SourceToken = paymentModel.StripeToken
-            };
+            if (string.IsNullOrWhiteSpace(stripeCustomerId))
+                {
+                var chargedCustomer = new StripeCustomerCreateOptions
+                {
+                    Email = currentMember.Email,
+                    Description = paymentDescription,
+                    SourceToken = paymentModel.StripeToken
+                };
 
-            //send these details to stripe to create the customer
-            stripeCustomer = customerService.Create(chargedCustomer);
+                //send these details to stripe to create the customer
+                stripeCustomer = customerService.Create(chargedCustomer);
 
-            //Save the new members id on the member
-            currentMember.SetValue("stripeCustomerID", stripeCustomer.Id);
-            }
+                //Save the new members id on the member
+                currentMember.SetValue("stripeCustomerID", stripeCustomer.Id);
+                }
             else
-            {
-            var chargedCustomer = new StripeCustomerUpdateOptions { SourceToken = paymentModel.StripeToken };
-            // update the customer with the token created earlier
+                {
+                var chargedCustomer = new StripeCustomerUpdateOptions { SourceToken = paymentModel.StripeToken };
+                // update the customer with the token created earlier
 
-            //send these details to stripe to create the customer
-            stripeCustomer = customerService.Update(StripeCustomerId, chargedCustomer);
-            }
+                //send these details to stripe to create the customer
+                stripeCustomer = customerService.Update(stripeCustomerId, chargedCustomer);
+                }
 
             // set a charge on the account based on the amount calaculated bellow
             var submitedCharge = new StripeChargeCreateOptions();
@@ -123,8 +152,11 @@ namespace SweetSerenityBySarah.Controllers
             submitedCharge.CustomerId = stripeCustomer.Id;
 
             //create the charge with the above details and charge the customer
-            var chargeService = new StripeChargeService(secretAPIKey);
+            var chargeService = new StripeChargeService(secretApiKey);
             var stripeCharge = chargeService.Create(submitedCharge);
+
+            TempData["paymentSuccess"] =
+                    "The payment has been processed and we will email you the details.";
 
             return CurrentUmbracoPage();
             }
